312fd8a531c30ebba96407679d3e68c9d794e16d
DacienNguyeen <daciennguyeen1904@gmail.com>
2024-10-22 14:35:06 +0700

    ex2

--- PF_NguyenTranTienDat/Session_4.cs
+++ PF_NguyenTranTienDat/Session_4.cs
@@ -1,15 +1,26 @@
 using System;
 namespace PF_NguyenTranTienDat
 {
-    internal class Loopsp
+    internal class Session_4_Operators
     {
-        static void ex1()
+        //1. Write a C# Sharp program that takes two numbers as input and
+        // performs an operation(+,-,*, x,/) on them and displays the result of that operation.
+
+        //Write a C# Sharp program to display certain values of the function x = y2 + 2y + 1
+        // (using integer numbers for y, ranging from -5 to +5).
+
+        static void ex2()
         {
-            Console.WriteLine("Hello bro");
+            int y, x;
+            for (y = -5; y <= 5; y++)
+            {
+                x = y2 + 2y + 1;
+            }
+            Console.WriteLine($"x = {x}");
         }
         static void Main(string[] args)
         {
-            ex1();
+            ex2();
             Console.ReadKey();
         }
     }
